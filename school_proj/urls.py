b87d7a6aaa3331d72f0794e8edd90ef4eb8d61b2
Pilovec <teperchuk-av@utg.ua>
2019-11-22 15:14:43 +0200

    make app blog and configure urls

--- school_proj/urls.py
+++ school_proj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zozh3.urls')),
+    path('blog/', include('blog.urls')),
 ]
--- zozh3/views.py
+++ zozh3/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'zozh3/index.html')
+
+
+
+
